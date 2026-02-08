6935cad4a0f91dd749022aa20238d3ea0443e505
Damikasulakshana <damika2223@gmail.com>
2026-02-08 19:20:05 +0530

    Test

---
+++ test.java
@@ -0,0 +1,11 @@
+public class byeworld{
+    public static void main(String[] args){
+        System.out.println("Bye World");
+    }
+}
+
+public class helloworld {
+    public static void main(string[] args){
+        system.out.println("Hello World");
+    }
+}
